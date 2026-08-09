48e91754d2e82d381cde7e195ea608cf097e7140
kunjansinh <kunjansinh90@gmail.com>
2026-08-10 00:39:00 +0100

    Add files via upload

---
+++ Week-1/HelloWorld.java
@@ -0,0 +1,13 @@
+// Name: Kunjan
+
+public class HelloWorld 
+{
+
+    public static void main(String[] args) 
+    {
+
+        System.out.println("Hello, World!");
+        System.out.println("My name is Kunjan.");
+        System.out.println("I am learning Java.");
+    }
+}
