8baaa7a5a8f022e497a9792ed9204c86331d2ab7
kunjansinh <kunjansinh90@gmail.com>
2026-08-12 23:25:31 +0100

    Add files via upload

---
+++ Week-1/UserInput.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// Name: Kunjan
+
+public class UserInput
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.println("\nHello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
+
+        input.close();
+    }
+}
