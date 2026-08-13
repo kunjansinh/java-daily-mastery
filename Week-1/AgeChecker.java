56ec2496f8bf4e8b254f672014f0f84a37b50909
kunjansinh <kunjansinh90@gmail.com>
2026-08-13 15:40:03 +0100

    Add files via upload

---
+++ Week-1/AgeChecker.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// Name: Kunjan
+
+public class AgeChecker
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 18)
+        {
+            System.out.println("You are an adult.");
+        }
+        else
+        {
+            System.out.println("You are a minor.");
+        }
+
+        input.close();
+    }
+}
