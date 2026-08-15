f8298c07521d38ef999483aa55bf578968318a8d
kunjansinh <kunjansinh90@gmail.com>
2026-08-15 22:17:49 +0100

    Add files via upload

---
+++ Week-1/GradeChecker.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+// Name: Kunjan
+
+public class GradeChecker
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your marks: ");
+        int marks = input.nextInt();
+
+        if (marks >= 70)
+        {
+            System.out.println("Grade: A");
+        }
+        else if (marks >= 60)
+        {
+            System.out.println("Grade: B");
+        }
+        else if (marks >= 50)
+        {
+            System.out.println("Grade: C");
+        }
+        else if (marks >= 40)
+        {
+            System.out.println("Grade: D");
+        }
+        else
+        {
+            System.out.println("Grade: F");
+        }
+
+        input.close();
+    }
+}
