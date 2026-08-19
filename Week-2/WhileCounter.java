96558691123df13bd132b271fd09528fc0f5ace3
kunjansinh <kunjansinh90@gmail.com>
2026-08-19 17:00:57 +0100

    Add files via upload

---
+++ Week-2/WhileCounter.java
@@ -0,0 +1,15 @@
+// Name: Kunjan
+
+public class WhileCounter
+{
+    public static void main(String[] args)
+    {
+        int number = 1;
+
+        while (number <= 10)
+        {
+            System.out.println(number);
+            number++;
+        }
+    }
+}
